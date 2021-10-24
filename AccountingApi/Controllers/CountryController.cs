77765df67a433e3b62fd8e836919c5a18ac2cf37
masaalt <82764723+masaalt@users.noreply.github.com>
2021-10-24 21:51:19 +0330

    Backend

---
+++ AccountingApi/Controllers/CountryController.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Framework.Enum;
+using Framework.ResultModel;
+using IRepository.Accounting;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using ViewModel.Accounting.ViewModels;
+using ViewModel.Accounting.ViewModels.Validator;
+
+namespace AccountingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly Result _result;
+        readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private readonly ICountryRepository _countryRepository;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="countryRepository"></param>
+        /// <param name="result"></param>
+        public CountryController(ICountryRepository countryRepository, Result result)
+        {
+            _countryRepository = countryRepository;
+            _result = result;
+        }
+
+
+        /// <summary>
+        /// مشاهده لیست کشور
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     api/countries
+        ///     {
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns>لیست کشور</returns>
+        /// <response code="200">موفق</response>
+        /// <response code="400">ناموفق</response>
+        /// <response code="401">عدم دسترسی</response>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var data = await _countryRepository.Get();
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                _logger.Fatal(ex, ex.Message);
+                _result.Message = ex.Message;
+                return BadRequest(_result);
+            }
+        }
+
+        /// <summary>
+        ///  درج کشور جدید
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     api/countries
+        ///     {
+        ///         "countryCode": "12345",
+        ///         "countryName": "ایران",
+        ///         "defaultCode": "12345",
+        ///         "population": 1,
+        ///         "area": 1,
+        ///         "distance": 1,
+        ///         "lenWid": 1,
+        ///         "capital": "تست",
+        ///         "time": "2019-08-05T18:54:26.276Z"
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns></returns>
+        /// <response code="200">موفق</response>
+        /// <response code="400">ناموفق</response>
+        /// <response code="401">عدم دسترسی</response>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CountryViewModel countryViewModel)
+        {
+            try
+            {
+                var validator = new CountryValidator();
+                var validationResult = validator.Validate(countryViewModel);
+                if (validationResult.IsValid)
+                {
+                    int res = await _countryRepository.Add(countryViewModel);
+                    if (res > 0)
+                    {
+                        return Ok(_result);
+                    }
+                    else
+                    {
+                        return BadRequest(_result);
+                    }
+                }
+                else
+                {
+                    if (_result.Errors == null)
+                        _result.Errors = new List<Error>();
+                    foreach (var validationError in validationResult.Errors)
+                        _result.Errors.Add(new Error()
+                        {
+                            Code = validationError.ErrorCode,
+                            Field = validationError.PropertyName,
+                            Title = "مقادیر ورودی معتبر نیستند",
+                            Detail = validationError.ErrorMessage,
+                        });
+                    _result.Message = "مقادیر ورودی معتبر نیستند";
+                    _result.Status = StatusType.BadRequest;
+                    return BadRequest(_result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Fatal(ex, ex.Message);
+                _result.Message = ex.Message;
+                return BadRequest(_result);
+            }
+        }
+
+        /// <summary>
+        ///  ویرایش کشور
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     api/countries
+        ///     {
+        ///         "countryId": 1,
+        ///         "countryCode": "12345",
+        ///         "countryName": "ایران",
+        ///         "defaultCode": "12345",
+        ///         "population": 1,
+        ///         "area": 1,
+        ///         "distance": 1,
+        ///         "lenWid": 1,
+        ///         "capital": "تست",
+        ///         "time": "2019-08-05T18:54:26.276Z"
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns></returns>
+        /// <response code="200">موفق</response>
+        /// <response code="400">ناموفق</response>
+        /// <response code="401">عدم دسترسی</response>
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] CountryViewModel countryViewModel)
+        {
+            try
+            {
+                var validator = new CountryValidator();
+                var validationResult = validator.Validate(countryViewModel);
+                if (validationResult.IsValid)
+                {
+                    int res = await _countryRepository.Update(countryViewModel);
+                    if (res > 0)
+                    {
+                        return Ok(_result);
+                    }
+                    else
+                    {
+                        return BadRequest(_result);
+                    }
+                }
+                else
+                {
+                    if (_result.Errors == null)
+                        _result.Errors = new List<Error>();
+                    foreach (var validationError in validationResult.Errors)
+                        _result.Errors.Add(new Error()
+                        {
+                            Code = validationError.ErrorCode,
+                            Field = validationError.PropertyName,
+                            Title = "مقادیر ورودی معتبر نیستند",
+                            Detail = validationError.ErrorMessage,
+                        });
+                    _result.Message = "مقادیر ورودی معتبر نیستند";
+                    _result.Status = StatusType.BadRequest;
+                    return BadRequest(_result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Fatal(ex, ex.Message);
+                _result.Message = ex.Message;
+                return BadRequest(_result);
+            }
+        }
+
+        /// <summary>
+        ///  حذف کشور
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     api/countries/1
+        ///     {
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns></returns>
+        /// <response code="200">موفق</response>
+        /// <response code="400">ناموفق</response>
+        /// <response code="401">عدم دسترسی</response>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                int res = await _countryRepository.Delete(id);
+                if (res > 0)
+                {
+                    return Ok(_result);
+                }
+                else
+                {
+                    return BadRequest(_result);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.Fatal(ex, ex.Message);
+                _result.Message = ex.Message;
+                return BadRequest(_result);
+            }
+        }
+    }
+}
---
+++ AccountingApi/Startup.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Framework.ResultModel;
+using IRepository.Accounting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using Repository.Accounting;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace AccountingApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            #region Dependency Injection
+
+            services.AddScoped<Result>();
+            services.AddTransient<ICountryRepository, CountryRepository>();
+
+            #endregion
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "حسابداری",
+                    Version = "v1",
+                    Description = "Acounting",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Parsina",
+                        Email = string.Empty,
+                        Url = string.Empty
+                    },
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
+                c.DescribeAllEnumsAsStrings();
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme()
+                {
+                    Description = "JWT Authorization header {token}",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                    { "Bearer", new string[] { } }
+                });
+            });
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors("AllowSpecificOrigin");
+
+            //add NLog to .NET Core
+            loggerFactory.AddNLog();
+
+            //Enable ASP.NET Core features (NLog.web) - only needed for ASP.NET Core users
+            app.AddNLogWeb();
+
+            //configure nlog.config in your project root. 
+            env.ConfigureNLog("nlog.config");
+
+            #region Swagger
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "سرویس ورژن 1 حسابداری");
+                c.DocExpansion(DocExpansion.None);
+                c.RoutePrefix = "swagger";
+            });
+            #endregion
+
+            app.UseMvc();
+        }
+    }
+}
